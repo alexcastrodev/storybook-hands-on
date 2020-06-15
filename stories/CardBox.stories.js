911ca990c2a32cdbb4621acd821af7032dddc5a1
Alex castro <alexoliveira7x@gmail.com>
2020-06-15 16:09:53 -0300

    adding props

--- stories/CardBox.stories.js
+++ stories/CardBox.stories.js
@@ -1,17 +1,20 @@
 import CardBox from './../src/components/CardBox';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default {
     title: 'CardBox',
     component: CardBox,
+    decorators: [withKnobs]
+
   };
 
   
 export const CardBoxComponent = () => ({
     components: {CardBox},
-    template: "<card-box msg='Hello World'/>",
+    template: "<card-box :msg='msg'/>",
     props: {
         msg: {
-          default: String('Hello World')
+          default: text('text','Hello World')
         },
     }
 })
